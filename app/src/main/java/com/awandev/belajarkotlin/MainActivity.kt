9abc5e5e840db8d66b0472f25eb353d77bc6dd5e
awalone <migoyeng@gmail.com>
2020-04-05 15:18:58 +0800

    2. Belajar Button, Snackbar, Toast

--- app/src/main/java/com/awandev/belajarkotlin/MainActivity.kt
+++ app/src/main/java/com/awandev/belajarkotlin/MainActivity.kt
@@ -2,11 +2,40 @@ package com.awandev.belajarkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    // fungsi yang pertamakali dieksekusi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn_click)
+        button.setOnClickListener { Toast.makeText(this, "Assalamu Alaykum", Toast.LENGTH_SHORT).show() }
+
+        val rootLayout = findViewById<LinearLayout>(R.id.root_layout)
+        val buttonSnack = findViewById<Button>(R.id.btn_click_snack)
+        buttonSnack.setOnClickListener { Snackbar.make(rootLayout, "Ini adalah snackbar", Snackbar.LENGTH_SHORT).show() }
+
+    }
+
+//    onPause adalah fungsi yang dipanggil ketika user menekan tombol HOME
+    override fun onPause() {
+        super.onPause()
+    }
+
+//    fungsi resume adalah fungsi yang akan dipanggil ketika user mengclick aplikasi kembali setelah menekan tombol home
+    override fun onResume() {
+        super.onResume()
+    }
+
+
+//    fungsi onDestroy adalah fungsi yang akan dipanggil ketika user mengklik tombol BACK
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
